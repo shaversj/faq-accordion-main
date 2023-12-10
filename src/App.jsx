50b17c5a9db744806896d4b2eed4cf78d3b72d2b
Josh Shavers <shaversj@gmail.com>
2023-12-10 09:49:26 -0600

    Mobile layout changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import iconStar from '/src/assets/images/icon-star.svg'
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className={"bg-black min-h-screen flex items-center"}>
+            <div className={"bg-green-400 max-w-screen-sm w-full h-[812px] mx-auto font-Work-Sans flex items-center"}>
+                <div className={"bg-white mx-6 my-10 px-6 rounded-lg"}>
+                    <header className={"flex items-center space-x-6 pt-4"}>
+                        <img src={iconStar} className={"w-[24px] h-[24px]"} alt={""} />
+                        <h1 className={"text-[32px] font-bold"}>FAQs</h1>
+                    </header>
+                    <main className={"divide-y-[0.5px] "}>
+                        <div className={"py-5"}>
+                            <details className={"bg-icon-p bg-no-repeat bg-right open:bg-icon-m open:bg-right-top"}>
+                                <summary className={"list-none pr-14 leading-[1.2rem] text-[16px] font-semibold text-dark-purple"}>What is Frontend Mentor, and how will it help me?</summary>
+                                <p className={"text-grayish-purple text-sm pt-6"}>
+                                    Frontend Mentor offers realistic coding challenges to help developers improve their
+                                    frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for
+                                    all levels and ideal for portfolio building.
+                                </p>
+
+                            </details>
+                        </div>
+
+                        <div className={"py-5"}>
+                            <details className={"bg-icon-p bg-no-repeat bg-right open:bg-icon-m open:bg-right-top "}>
+                                <summary className={"list-none pr-14 text-[16px] font-semibold text-dark-purple"}>Is Frontend Mentor free?</summary>
+                                <p className={"text-grayish-purple text-sm pt-6"}>
+                                    Yes, Frontend Mentor offers both free and premium coding challenges, with the free
+                                    option providing access to a range of projects suitable for all skill levels.
+                                </p>
+
+                            </details>
+                        </div>
+
+                        <div className={"py-5"}>
+                            <details className={"bg-icon-p bg-no-repeat bg-right open:bg-icon-m open:bg-right-top "}>
+                                <summary className={"list-none pr-14 leading-[1.2rem] text-[16px] font-semibold text-dark-purple"}>Can I use Frontend Mentor projects in my portfolio?</summary>
+                                <p className={"text-grayish-purple text-sm pt-6"}>
+                                    Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent
+                                    way to showcase your skills to potential employers!
+                                </p>
+
+                            </details>
+                        </div>
+
+                        <div className={"py-5"}>
+                            <details className={"bg-icon-p bg-no-repeat bg-right open:bg-icon-m open:bg-right-top "}>
+                                <summary className={"list-none pr-14 leading-[1.2rem] text-[16px] font-semibold text-dark-purple"}>How can I get help if I'm stuck on a challenge?</summary>
+                                <p className={"text-grayish-purple text-sm pt-6"}>
+                                    The best place to get help is inside Frontend Mentor's Discord community. There's a help
+                                    channel where you can ask questions and seek support from other community members.
+                                </p>
+
+                            </details>
+                        </div>
+
+                    </main>
+
+                </div>
+            </div>
+
+        </div>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,17 @@ export default {
       'lg': '1440px'
     },
     extend: {
+      backgroundImage: {
+        'icon-p': "url('/src/assets/images/icon-plus.svg')",
+        'icon-m': "url('/src/assets/images/icon-minus.svg')"
+      },
       colors: {
         'light-pink': "hsl(275, 100%, 97%)",
         'grayish-purple': "hsl(292, 16%, 49%)",
         'dark-purple': "hsl(292, 42%, 14%)",
+      },
+      fontFamily: {
+        'Work-Sans': ['Work Sans', 'sans-serif']
       }
     },
   },
